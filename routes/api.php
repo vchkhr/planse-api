d98b60dd2a2bec056d6212967be4d9d0a4971fbb
Viacheslav Kharchenko <vchkhr@gmail.com>
2021-09-08 09:37:43 +0300

    Add names to routes and update env example

--- routes/api.php
+++ routes/api.php
@@ -15,10 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('register', [AuthController::class, 'register']);
-Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register'])->name('register');
+
+Route::get('login', [AuthController::class, 'login'])->name('login');
+Route::post('login', [AuthController::class, 'login'])->name('login');
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('user', [AuthController::class, 'user']);
-    Route::post('logout', [AuthController::class, 'logout']);
+    Route::get('user', [AuthController::class, 'user'])->name('user');
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
 });

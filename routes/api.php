2c162c56a390cd7a3b59bb26159507093fb34e52
Viacheslav Kharchenko <vchkhr@gmail.com>
2021-09-09 14:39:51 +0300

    Get Calendar information

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CalendarController;
 use App\Http\Controllers\TestController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -26,4 +28,6 @@ Route::post('login', [AuthController::class, 'login'])->name('login');
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('user', [AuthController::class, 'user'])->name('user');
     Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+
+    Route::get('/calendar/{calendar}', [CalendarController::class, 'show']);
 });

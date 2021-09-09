62f8807e78247980e8709f968e0e68a80bea626b
Viacheslav Kharchenko <vchkhr@gmail.com>
2021-09-09 22:48:34 +0300

    Fix middleware

---
+++ app/Http/Middleware/Api.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class API
+{
+
+    public function handle($request, Closure $next)
+    {
+
+        $response = $next($request);
+        $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Content-Range, Content-Disposition, Content-Description, X-Auth-Token');
+        $response->header('Access-Control-Allow-Origin', '*');
+        return $response;
+    }
+}
